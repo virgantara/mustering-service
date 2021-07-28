b4ab2f64dd65cac60e79cb0726ecac27a7bf8367
virgantara <oddy@unida.gontor.ac.id>
2021-07-28 10:47:56 +0700

    fix

--- app/model/appModel.js
+++ app/model/appModel.js
@@ -14,15 +14,18 @@ var Mustering = function(task){
 
 
 function getPresensi(dataQuery, callback){
-    let params = [dataQuery.sd, dataQuery.ed, dataQuery.jenis]
+    let params = [dataQuery.sd, dataQuery.ed]
 
     let txt = "SELECT t.id, t.kondisi_saat_ini, p.nama, p.nrp, t.tempat_tinggal_lat, t.tempat_tinggal_lon "
-    txt += ", no_hp, no_wa, pk.nama as pangkat, p.jenis_pegawai, d.nama as dinas, p.satuan, t.kondisi_saat_ini as kondisi "
+    txt += ", no_hp, no_wa, pk.nama as pangkat, p.jenis_pegawai, d.nama as dinas, p.satuan, t.kondisi_saat_ini as kondisi, t.pegawai_id as pid "
+    txt += ", u.email "
     txt += " FROM presensi t "
+    
     txt += " LEFT JOIN pegawai p ON t.pegawai_id = p.id "
     txt += " LEFT JOIN pangkat pk ON p.pangkat_id = pk.id "
     txt += " LEFT JOIN dinas d ON p.dinas_id = d.id "
-    txt += " WHERE (t.created_at BETWEEN ? AND ?) AND p.jenis_pegawai = ? "
+    txt += " LEFT JOIN user u ON t.pegawai_id = u.pegawai_id "
+    txt += " WHERE (t.created_at BETWEEN ? AND ?) "
     txt += " ORDER BY t.pegawai_id, t.created_at DESC limit 50 "
     
     sql.query(txt,params,function(err, res){
